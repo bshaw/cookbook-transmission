2887b3a09757f85bf0f7af4891e71fd40d4a4e67
Brian Shaw <bshaw@virtusa.com>
2016-06-24 14:08:39 +0100

    Fix cookbook name

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
-name 'cookbook-transmission'
+name 'transmission'
 maintainer 'Brian Shaw'
 maintainer_email 'brian.b.shaw@gmail.com'
 license 'all_rights'
 description 'Installs/Configures the Transmission BitTorrent client as a Docker container'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
+version '0.1.1'
 
 depends 'docker'
 depends 'firewall'
